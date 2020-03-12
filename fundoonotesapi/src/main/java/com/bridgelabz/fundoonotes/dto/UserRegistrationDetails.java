2a83cce334e015eae7d57923080b4596fc3e6e46
iamshaiksha <iamshaiksha2019@gmail.com>
2020-03-12 09:51:38 +0530

    [shaiksha]refractor.changes done

--- fundoonotesapi/src/main/java/com/bridgelabz/fundoonotes/dto/UserRegistrationDetails.java
+++ fundoonotesapi/src/main/java/com/bridgelabz/fundoonotes/dto/UserRegistrationDetails.java
@@ -1,44 +1,22 @@
 package com.bridgelabz.fundoonotes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * @author shaik shaiksha vali
  * 
  * */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserRegistrationDetails {
 	private String name;
 	private String email;
 	private String password;
 	private String mobileNumber;
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
 
 }
