072126dbc15e3bd7a05fee6ed51cbecf857ba92e
iamshaiksha <iamshaiksha2019@gmail.com>
2020-03-16 08:35:12 +0530

    [shaiksha] refractor.changes done

---
+++ src/main/java/com/bridgelabz/fundoonotes/model/ProfilePic.java
@@ -0,0 +1,60 @@
+package com.bridgelabz.fundoonotes.model;
+
+/*
+ *  author : shaiksha
+ */
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Entity
+@Data
+/* Entity Class for profilePic */
+public class ProfilePic {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String profilePicName;
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private UserInformation userLabel;
+
+	public ProfilePic(String fileName, UserInformation user) {
+		this.profilePicName = fileName;
+		this.userLabel = user;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProfilePicName() {
+		return profilePicName;
+	}
+
+	public void setProfilePicName(String profilePicName) {
+		this.profilePicName = profilePicName;
+	}
+
+	public UserInformation getUserLabel() {
+		return userLabel;
+	}
+
+	public void setUserLabel(UserInformation userLabel) {
+		this.userLabel = userLabel;
+	}
+
+}
