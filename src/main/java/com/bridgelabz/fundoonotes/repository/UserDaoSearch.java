cd13273bc6b0f2d1fbd40403878158a1040deb7f
iamshaiksha <iamshaiksha2019@gmail.com>
2020-03-16 08:36:23 +0530

    [shaiksha] refractor.changes done

---
+++ src/main/java/com/bridgelabz/fundoonotes/repository/UserDaoSearch.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.fundoonotes.repository;
+
+import java.util.List;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.bridgelabz.fundoonotes.model.UserInformationSearch;
+public interface UserDaoSearch extends ElasticsearchRepository<UserInformationSearch, String>{
+	List<UserInformationSearch> findbyName(String name);
+
+}
