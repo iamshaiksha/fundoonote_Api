6f1dc2e3a85dce061dab65552d658f08ea5d4d2b
iamshaiksha <iamshaiksha2019@gmail.com>
2020-03-16 08:30:23 +0530

    [shaiksha] refractor.changes done

--- src/main/java/com/bridgelabz/fundoonotes/controller/Collaborator.java
+++ src/main/java/com/bridgelabz/fundoonotes/controller/Collaborator.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,7 +38,7 @@ public class Collaborator {
 	 * @param noteId
 	 * @param email
 	 * @param token
-	 * @return
+	 * @return noteInformation
 	 */
 	//Api for adding collaborator
 	@PostMapping("Collaborate/addcollaborator/{noteId}/{email}")
@@ -53,7 +52,7 @@ public class Collaborator {
 	/**
 	 * 
 	 * @param token
-	 * @return
+	 * @return List<NoteInformation>
 	 */
 	//Api for fetching All collaborators
 	@GetMapping("collaborator/getAllcollaborators")
@@ -69,7 +68,7 @@ public class Collaborator {
 	 * @param noteId
 	 * @param email
 	 * @param token
-	 * @return
+	 * @return noteInformation
 	 */
 	////Api for deleting collaborator
 	@DeleteMapping("collaborator/deleteCollaborator")
